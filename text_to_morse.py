1fd48ab71460a6aec991ad372ca07e7e3612838e
nicheweb <norgerojascerulia@gmail.com>
2023-06-03 17:49:17 +0200

    UI--> Clear the screen everytime runs the prog and says good bye

--- text_to_morse.py
+++ text_to_morse.py
@@ -1,7 +1,14 @@
+import os
 from morse_class import Morse
 from morse_art import logo
+from time import sleep
 
+def clear():
+    # check and runs the command for the specific operating system
+    command = 'clear' if os.name == 'posix' else 'cls'
+    os.system(command)
 def run_main_prog():
+    clear()
     print(logo)
     text = input(
         'Enter text(Only letters and numbers from the american alphabet) '
@@ -14,6 +21,10 @@ def run_main_prog():
     end_program = True if run_again == 'n' else False
     if not end_program:
         run_main_prog()
+    print('GOOD BYE...')
+    sleep(3)
+    clear()
+    exit()
 
 if __name__ == '__main__':
     run_main_prog()
