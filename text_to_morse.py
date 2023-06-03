1423e4b31933ee388ab84d1821947f34b58313b7
nicheweb <norgerojascerulia@gmail.com>
2023-06-03 14:07:22 +0200

    Add ASCII art

--- text_to_morse.py
+++ text_to_morse.py
@@ -1,23 +1,19 @@
 from morse_class import Morse
-
+from morse_art import logo
 
 def run_main_prog():
+    print(logo)
     text = input(
         'Enter text(Only letters and numbers from the american alphabet) '
         'and press Enter: \n'
     ).upper()
     morse = Morse(text)
     print(f'\nMORSE TEXT:\n{morse.morse_text}')
+    run_again = input(
+        '\nDo you wanna run the program again? (y/n): ').lower()
+    end_program = True if run_again == 'n' else False
+    if not end_program:
+        run_main_prog()
 
 if __name__ == '__main__':
-    end_program = False
-    while not end_program:
-        run_main_prog()
-        run_again = input('\nDo you wanna run the program again? (y/n): ').lower()
-        end_program = True if run_again == 'n' else False
-
-
-
-
-
-
+    run_main_prog()
